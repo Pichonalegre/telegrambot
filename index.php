1901aac80d3dac64336926e7936620f1884e2e12
Aladin <aqh0001@alu.medac.es>
2022-02-14 17:29:12 +0100

    a

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
     $chatId = $update["message"]["chat"]["id"];
     $message = $update["message"]["text"];
     
+    if (strpos($message, "/start") === 0) {
+        
+        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Bienvenido");
+    }
     
-    /*
     if (strpos($message, "/weather") === 0) {
         $location = substr($message, 9);
         $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
         file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
-    }*/
-    echo "holaa";
+    }
 ?>
