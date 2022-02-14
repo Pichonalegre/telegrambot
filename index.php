fb35778d9d48d11a59804c3b02ab4bcd065ee78f
Aladin <aqh0001@alu.medac.es>
2022-02-14 18:50:45 +0100

    comando tiempo funciona, pero regular

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-   $path = "https://api.telegram.org/bot5224778305:AAF6INif-5hT8WgcjTtaUAdIh63iItyPDcA";
+   $token = "https://api.telegram.org/bot5224778305:AAF6INif-5hT8WgcjTtaUAdIh63iItyPDcA";
    $update = json_decode(file_get_contents("php://input"), TRUE);
 
    $chatId = $update["message"]["chat"]["id"];
@@ -7,12 +7,12 @@
 
    if (strpos($message, "/start") === 0) {
 
-     file_get_contents($path."/sendMessage?chat_id=".$chatId."&text=Bienvenido");
+     file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=Bienvenido");
    }
     
    if (strpos($message, "/weather") === 0) {
       $location = substr($message, 9);
-      $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
-      file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Este es el tiempo en ".$location.": ". $weather);
+      $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=token"), TRUE)["weather"][0]["main"];
+      file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=Este es el tiempo en ".$location.": ". $weather);
    }
 ?> 
