2912efebe932988953a9f1c61bda701257bed3c5
Aladin <aqh0001@alu.medac.es>
2022-02-14 18:37:35 +0100

    prueba tiempo 2

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-    $path = "https://api.telegram.org/bot5224778305:AAF6INif-5hT8WgcjTtaUAdIh63iItyPDcA";
-    $update = json_decode(file_get_contents("php://input"), TRUE);
+   $path = "https://api.telegram.org/bot5224778305:AAF6INif-5hT8WgcjTtaUAdIh63iItyPDcA";
+   $update = json_decode(file_get_contents("php://input"), TRUE);
 
-    $chatId = $update["message"]["chat"]["id"];
-    $message = $update["message"]["text"];
+   $chatId = $update["message"]["chat"]["id"];
+   $message = $update["message"]["text"];
+   
+   if (strpos($message, "/start") === 0) {
 
-    if (strpos($message, "/start") === 0) {
-
-        file_get_contents($path."/sendMessage?chat_id=".$chatId."&text=Bienvenido");
-    }
+     file_get_contents($path."/sendMessage?chat_id=".$chatId."&text=Bienvenido");
+   }
     
-    if (strpos($message, "/tiempo") === 0) {
-        $location = substr($message, 9);
-        $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
-        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
-    }
+   if (strpos($message, "/tiempo") === 0) {
+     $location = substr($message, 9);
+     $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+     file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+   }
 ?> 
