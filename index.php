7da0ddc8b4e3286bac9f45974208a8044562760a
Aladin <aqh0001@alu.medac.es>
2022-02-14 18:41:14 +0100

    comando tiempo funciona, pero regular

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
    }
     
    if (strpos($message, "/tiempo") === 0) {
-     $location = substr($message, 10);
+     $location = substr($message, 8);
      $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
      file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
    }
