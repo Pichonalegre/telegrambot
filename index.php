366020de013e22efd86e58751994f74b454534da
Aladin <aqh0001@alu.medac.es>
2022-02-16 18:58:14 +0100

    creo que funciona con cualquier ciudad

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     
    if (strpos($message, "/tiempo") === 0) {
       $location = substr($message, 8);
-      $weather = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/forecast?id=6357751&hourly?lat=36.976369&lon=-3.540951&&appid=5cf939c3ecb138f202af77717ecbe12e"), TRUE)["weather"][0]["main"];
+      $weather = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&callback=test&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["weather"][0]["main"];
       file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=Este es el tiempo en ".$location.": ". $weather);
    }
 ?> 
