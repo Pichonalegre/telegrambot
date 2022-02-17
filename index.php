0f8105916eb94d831cd477e9945f89f93f0c9343
Aladin <aqh0001@alu.medac.es>
2022-02-17 20:01:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
    if (strpos($message, "/start") === 0) {
       file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=Bienvenid@!" );
       file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=Comandos:");
-      file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=Comandos:");
-      file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=/Stream Visita mis streams en twitch");
+      file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=/stream Visita mis streams en twitch");
       file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=/tiempo \"localizacion\" consulta el tiempo");
       file_get_contents($token."/sendMessage?chat_id=".$chatId."&text=/tarkov \"item\" Consulta el precio del item introducido en el juego Tarkov");
    }
