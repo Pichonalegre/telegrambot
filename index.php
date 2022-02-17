18dd53b17bbf14377a7153503adf94095c4ebf60
Aladin <aqh0001@alu.medac.es>
2022-02-17 04:21:22 +0100

    Update index.php
    añado noticias

--- index.php
+++ index.php
@@ -23,16 +23,16 @@
       $weather1 = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["weather"][0]["main"];
       $weather2 = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["weather"][0]["description"];
       $weather3 = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["wind"]["speed"];
-      $tarkov = json_decode(file_get_contents("https://tarkov-market.com/api/v1/item?q=btc&x-api-key=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)[""]["price"];
       file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=Este es el tiempo en ".$location.": ". $weather1);
       file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=Descripcion: ". $weather2);
       file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=Velocidad: ". $weather3);
 
    }
 
-   if (strpos($message, "/tarkov") === 0) {
+   if (strpos($message, "/news") === 0) {
       $item = substr($message, 8);
-      file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=precio de ".$item.": ". $tarkov);
+      $news = json_decode(file_get_contents("http://api.mediastack.com/v1/news?access_key=85b7a0cec862c55c0bd53253323aef03"), TRUE)["data"]["title"];
+      file_get_contents($token."/sendmessage?chat_id=".$chatId."&text=noticias: ". $news);
    }
 
 ?> 
