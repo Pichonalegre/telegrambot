5162face6bd78b35a46f4731dbe78e6885674bfd
Aladin <aqh0001@alu.medac.es>
2022-02-21 20:13:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
   
    if (strpos($message, "/tiempo") === 0) {
       $location = substr($message, 8);
-   
-      sendMessage($chatid, $location, false);
+      
+      sendMessage($chatid, $location, true);
 
    
       $localizacion=json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&lang=es&units=metric&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["name"];
@@ -30,11 +30,11 @@
       $weather4 = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&lang=es&units=metric&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["main"]["temp_max"];
       $weather5 = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&lang=es&units=metric&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["main"]["temp_min"];
 
-      file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=Este es el tiempo en ".$localizacion.": ". $weather1);
-      file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=Descripción: ". $weather2);
-      file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=Velocidad: ". $weather3."Km/h");
-      file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=temperatura maxima: ". $weather4."º");
-      file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=temperatura minima: ". $weather5."º");
+      // file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=Este es el tiempo en ".$localizacion.": ". $weather1);
+      // file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=Descripción: ". $weather2);
+      // file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=Velocidad: ". $weather3."Km/h");
+      // file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=temperatura maxima: ". $weather4."º");
+      // file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=temperatura minima: ". $weather5."º");
    }
 
    if (strpos($message, "/news") === 0) {
