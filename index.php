00e2928126d5c13b4c3913fb2bda19f1eede9e81
Aladin <aqh0001@alu.medac.es>
2022-02-21 20:41:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
   
    if (strpos($message, "/tiempo") === 0) {
       $location = substr($message, 8);
-      $respuesta=file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=¿tiempo de que ciudad?");
-
+      file_get_contents($token."/sendmessage?chat_id=".$chatid."&text=¿tiempo de que ciudad?");
+      $respuesta="¿tiempo de que ciudad?";
       sendMessage($chatid, $respuesta, true);
 
       $localizacion=json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$location."&lang=es&units=metric&appid=a32b06b98aa8fdc06e5902d229eb2055"), TRUE)["name"];
